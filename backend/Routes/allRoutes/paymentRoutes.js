9638ad43584a30a1df681d84ba7347757ffd34fc
Arun Rajput <arun@bastionex.net>
2025-07-02 18:31:47 +0530

    gateway redirect urls

--- backend/Routes/allRoutes/paymentRoutes.js
+++ backend/Routes/allRoutes/paymentRoutes.js
@@ -33,8 +33,8 @@ router.post('/create-checkout-session', async (req, res) => {
                     quantity: 1,
                 },
             ],
-            success_url: `http://localhost:3000/invoice?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: 'http://localhost:3000',
+            success_url: `${process.env.FRONTEND_URL2}/invoice?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.FRONTEND_URL2}`,
             metadata: {
                 batchId,
                 quantity,
